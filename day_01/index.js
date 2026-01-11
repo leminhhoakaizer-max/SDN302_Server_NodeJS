1544758bac51d1334db6a791b190863e50bdb8af
LeMinhHoa <leminhhoakaizer@gmail.com>
2026-01-11 09:14:27 +0700

    Add production server URL to Swagger

--- day_01/index.js
+++ day_01/index.js
@@ -1,17 +1,21 @@
+// Load environment variable (.env)
 import 'dotenv/config';
-
 import express from 'express'
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
+// Database
 import { DBConnection } from './src/config/DBContext.js';
+
+// Routers
 import authRouter from './src/routes/authenRT.routes.js';
 import profileRouter from './src/routes/profile.routes.js';
 import productRouter from './src/routes/product.routes.js';
 import categoryRouter from './src/routes/categories.routes.js';
 import cartRouter from './src/routes/cart.routes.js';
 
+// Swagger
 import { setupSwagger } from './src/config/swagger.js';
 
 const app = express()
@@ -19,14 +23,17 @@ const app = express()
 // Call to [Connection database]
 DBConnection();
 
-// Middleware for parsing request body
+// Parse cookies
 app.use(cookieParser());
+
+// Parse JSON && form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Static files(images)
 app.use('/images', express.static(path.join(process.cwd(), 'images')));
 
+// CORS configuration
 app.use(cors({
   origin: '*', // Cho phép tất cả domain truy cập
   method: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -36,17 +43,32 @@ app.use(cors({
 // Set-up Swagger
 setupSwagger(app);
 
+// API routes
 app.use('/api/authen', authRouter); // [authenRT.routes.js]
 app.use('/api/profile', profileRouter); // [profile.routes.js]
 app.use('/api/enhancedProduct', productRouter) // [product.routes.js]
 app.use('/api/enhancedcategories', categoryRouter) // [categories.routes.js]
 app.use('/api/cart', cartRouter) // [cart.routes.js]
-// Routes
+
+// Root route (health check: kiểm tra sức khỏe + API Infor)
 app.get('/', (req, res) => {
-  res.send('Home pages')
-})
+  res.json({
+    message: "SDN302 API is running!",
+    swagger: "/api-docs",
+    endpoint: {
+      authen: '/api/authen',
+      profile: '/api/profile',
+      enhancedProduct: '/api/enhancedProduct',
+      enhancedcategories: '/api/enhancedcategories',
+      cart: '/api/cart',
+    }
+  });
+});
+
+// User PORT from Vercel environment || fallback to 3000
+const PORT = process.env.PORT || 3000;
 
 // Start server
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000")
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
